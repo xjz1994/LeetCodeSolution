b2dbdbcde7775ad10e277430774aa8b20b7c53a1
xjz1994 <504767600@qq.com>
2017-11-06 20:39:07 +0800

    655.print binary tree

--- Solution/655.Print Binary Tree.js
+++ Solution/655.Print Binary Tree.js
@@ -1,3 +1,5 @@
+let Tree = require('../Helper/Javascript/Tree').Tree;
+
 /**
  * Definition for a binary tree node.
  * function TreeNode(val) {
@@ -6,16 +8,29 @@
  * }
  */
 
-
-let Tree = require('../Helper/Javascript/Tree').Tree;
-
 /**
  * @param {TreeNode} root
  * @return {string[][]}
  */
 var printTree = function (root) {
-
+    let rowNum = maxDepth(root);
+    let colNum = Math.pow(2, rowNum - 1) * 2 - 1;
+    let res = [];
+    for (let i = 0; i < rowNum; i++) {
+        res[i] = [];
+        for (let j = 0; j < colNum; j++) {
+            res[i][j] = "";
+        }
+    }
+    console.log(res);
 };
 
-let root = Tree.CreateTree([1, 2, 3, null, 4, null, null, null, null, 1, 2,]);
-Tree.BFSRead(root);
+var maxDepth = function (root) {
+    if (!root) return 0;
+    let left = maxDepth(root.left);
+    let right = maxDepth(root.right);
+    return Math.max(left, right) + 1;
+}
+
+let root = Tree.CreateTree([1, 2, 5, 3, null, null, null, 4, 5]);
+console.log(printTree(root));
