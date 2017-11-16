00263d656d41e6dc34c5c66d75f1a2ab66c10375
xjz1994 <504767600@qq.com>
2017-11-16 15:48:36 +0800

    725

--- Solution/725.Split Linked List in Parts.js
+++ Solution/725.Split Linked List in Parts.js
@@ -2,8 +2,26 @@ let LinkList = require('../Helper/Javascript/LinkList').LinkList;
 
 
 var splitListToParts = function (root, k) {
-
+    let nodeArr = [];
+    let node = root;
+    while (node) {
+        nodeArr.push(node.val);
+        node = node.next;
+    }
+    let res = [];
+    let startIndex = 0;
+    let mod = nodeArr.length % k;
+    let quo = Math.floor(nodeArr.length / k);
+    for (let i = 0; i < k; i++) {
+        let length = mod-- > 0 ? quo + 1 : quo;
+        res.push(nodeArr.slice(startIndex, startIndex + length));
+        startIndex += length;
+    }
+    return res;
 };
 
-let root = LinkList.CreateLinkList([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
-let res = splitListToParts(root, 3)
+// let root = LinkList.CreateLinkList([]);
+// let res = splitListToParts(root, 3)
+// let root = LinkList.CreateLinkList([1, 2, 3]);
+// let res = splitListToParts(root, 5)
+
