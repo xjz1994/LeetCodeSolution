8acf62f936eab83461cb586bdf430f095066e50a
xjz1994 <504767600@qq.com>
2017-12-03 19:20:30 +0800

    1

--- Solution/CSharp/1.Sum.cs
+++ Solution/CSharp/1.Sum.cs
@@ -1,17 +1,17 @@
 public class Solution {
     public int[] TwoSum(int[] nums, int target) {
-		if (nums == null || nums.Length < 1) {
-			return null;
-		}
-		Dictionary<int, int> dict = new Dictionary<int, int>();
-		for (int i = 0; i < nums.Length; i++) {
-			if (dict.ContainsKey(target - nums[i])) {
-				int[] arr = { dict[target - nums[i]], i};
-				return arr;
-			} else {
-				dict[nums[i]] = i;
+			if (nums == null || nums.Length < 1) {
+				return null;
 			}
-		}
-		return null;
+			Dictionary<int, int> dict = new Dictionary<int, int>();
+			for (int i = 0; i < nums.Length; i++) {
+				if (dict.ContainsKey(target - nums[i])) {
+					int[] arr = { dict[target - nums[i]], i};
+					return arr;
+				} else {
+					dict[nums[i]] = i;
+				}
+			}
+			return null;
     }
 }
