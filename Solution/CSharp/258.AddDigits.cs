ff2f77907ac010da4ab00e3359f8f991311b9a02
xjz1994 <504767600@qq.com>
2017-12-05 11:32:34 +0800

    fix AddDigits

--- Solution/CSharp/258.AddDigits.cs
+++ Solution/CSharp/258.AddDigits.cs
@@ -1,22 +1,14 @@
-static public int Add(int x, int y) {//=B5=DD=B9=E9
-	return y == 0 ? x : Add(x ^ y, (x &amp; y) << 1);
-}
-
-static public int Add(int x, int y) {//=B5=DD=B9=E9
-	if (y == 0) {
-		return x;
-	}
-	int sum = x ^ y;
-	int carry = (x &amp; y) << 1;
-	return Add(sum, carry);
-}
-
-static public int Add(int x, int y) {//=D1=AD=BB=B7
-	int sum = 0;
-	while (y != 0) {
-		sum = x ^ y;
-		y = (x &amp; y) << 1;
-		x = sum;
-	}
-	return sum;
+public class Solution {
+    public int AddDigits(int num) {
+        char[] nums = num.ToString().ToCharArray();
+        if(num/10 >= 1){
+            int sum = 0;
+            foreach(char c in nums){
+                sum += (Convert.ToInt32(c) - 48);
+            }
+            return AddDigits(sum);
+        }else{
+            return num;
+        }
+    }
 }
