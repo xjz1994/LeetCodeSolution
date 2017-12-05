3ca5aaf3c9295f52586b39c3cd7866701bf55f86
xjz1994 <504767600@qq.com>
2017-12-05 15:02:38 +0800

    258

--- Solution/CSharp/258.AddDigits.cs
+++ Solution/CSharp/258.AddDigits.cs
@@ -1,14 +1,56 @@
-public class Solution {
-    public int AddDigits(int num) {
-        char[] nums = num.ToString().ToCharArray();
-        if(num/10 >= 1){
-            int sum = 0;
-            foreach(char c in nums){
-                sum += (Convert.ToInt32(c) - 48);
+/*
+    Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+
+    For example:
+
+    Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
+
+    Follow up:
+    Could you do it without any loop/recursion in O(1) runtime? 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Solution
+{
+
+    public class Solution
+    {
+        public int AddDigits(int num)
+        {
+            char[] nums = num.ToString().ToCharArray();
+            if (num / 10 >= 1)
+            {
+                int sum = 0;
+                foreach (char c in nums)
+                {
+                    sum += (Convert.ToInt32(c) - 48);
+                }
+                return AddDigits(sum);
             }
-            return AddDigits(sum);
-        }else{
-            return num;
+            else
+            {
+                return num;
+            }
+        }
+
+        //one line solution
+        public int AddDigits(int num)
+        {
+            return (num - 1) % 9 + 1;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new Solution();
+            var res = s.AddDigits(38);
+            Console.WriteLine(res);
         }
     }
 }
