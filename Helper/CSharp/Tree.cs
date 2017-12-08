e89199f9aee5a00640d19734c043928206122db2
xjz1994 <504767600@qq.com>
2017-12-08 17:05:12 +0800

    maxdepth of binary tree

--- Helper/CSharp/Tree.cs
+++ Helper/CSharp/Tree.cs
@@ -5,23 +5,26 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Algorithm {
-	class TreeNode {
-	     public int val;
-	     public TreeNode left;
-	     public TreeNode right;
-	     public TreeNode(int x) { val = x; }
-	 }
-	class Tree {
+	public class TreeNode {
+		public int val;
+		public TreeNode left;
+		public TreeNode right;
+		public TreeNode(int x) { val = x; }
+	}
+
+	public class Tree {
+
 		public static TreeNode CreateNode(int? val) {
 			if (val == null) return null;
-			return new TreeNode((int)val);
+			return new TreeNode((int) val);
 		}
+
 		public static TreeNode CreateTree(int?[] arr) {
 			if (arr.Length <= 0 || arr[0] == null) {
 				return null;
 			}
 			TreeNode root = Tree.CreateNode(arr[0]);
-			Queue<TreeNode> queue = new Queue<TreeNode>();
+			var queue = new Queue<TreeNode>();
 			queue.Enqueue(root);
 			int index = 1;
 			while (queue.Count > 0) {
@@ -38,6 +41,7 @@ namespace Algorithm {
 			}
 			return root;
 		}
+
 		public static void Walk(TreeNode node, Action<TreeNode> func, TreeWalkType type) {
 			if (node != null) {
 				if (type == TreeWalkType.Pre) func(node);
@@ -47,10 +51,11 @@ namespace Algorithm {
 				if (type == TreeWalkType.Post) func(node);
 			}
 		}
-		//非递归，广度优先遍历
+
+		//广度优先遍历
 		public static void BFSWalk(TreeNode root, Action<TreeNode> func) {
 			if (root == null) return;
-			Queue<TreeNode> queue = new Queue<TreeNode>();
+			var queue = new Queue<TreeNode>();
 			queue.Enqueue(root);
 			while (queue.Count > 0) {
 				TreeNode node = queue.Dequeue();
@@ -59,12 +64,42 @@ namespace Algorithm {
 				if (node.right != null) queue.Enqueue(node.right);
 			}
 		}
-		//非递归，中序遍历
+
+		//广度优先,逐层遍历
+		public static void BFSWalkLevel(TreeNode root, Action<TreeNode> func) {
+			if (root == null) return;
+			var queue = new Queue<TreeNode>();
+			queue.Enqueue(root);
+			while (queue.Count > 0) {
+				int count = queue.Count;
+				for (int i = 0; i < count; i++) {
+					TreeNode node = queue.Dequeue();
+					func(node);
+					if (node.left != null) queue.Enqueue(node.left);
+					if (node.right != null) queue.Enqueue(node.right);
+				}
+			}
+		}
+
+		//深度优先，先序
+		public static void DFSWalk(TreeNode root, Action<TreeNode> func) {
+			if (root == null) return;
+			var stack = new Stack<TreeNode>();
+			stack.Push(root);
+			while (stack.Count > 0) {
+				var node = stack.Pop();
+				func(node);
+				if (node.right != null) stack.Push(node.right);
+				if (node.left != null) stack.Push(node.left);
+			}
+		}
+
+		//深度优先，中序
 		public static void InOrderTreeWalk(TreeNode root, Action<TreeNode> func) {
 			if (root == null) {
 				return;
 			}
-			Stack<TreeNode> stack = new Stack<TreeNode>();
+			var stack = new Stack<TreeNode>();
 			TreeNode current = root;
 			while (current != null) {
 				stack.Push(current);
@@ -72,7 +107,7 @@ namespace Algorithm {
 			}
 			while (stack.Count != 0) {
 				current = stack.Pop();
-				func(current);             //func
+				func(current); //func
 				TreeNode node = current.right;
 				while (node != null) {
 					stack.Push(node);
@@ -81,7 +116,8 @@ namespace Algorithm {
 			}
 		}
 	}
-	enum TreeWalkType {
+
+	public enum TreeWalkType {
 		Pre,
 		In,
 		Post
--- Solution/CSharp/104.MaximumDepthofBinaryTree.cs
+++ Solution/CSharp/104.MaximumDepthofBinaryTree.cs
@@ -1,8 +1,53 @@
-public int MaxDepth(TreeNode root) {
-	if (root == null) {
-		return 0;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Algorithm;
+
+namespace Solution {
+
+	public class Solution1 {
+		public int MaxDepth(TreeNode root) {
+			if (root == null) return 0;
+			int left = MaxDepth(root.left) + 1;
+			int right = MaxDepth(root.right) + 1;
+			return left > right ? left : right;
+		}
 	}
-	int left = MaxDepth(root.left) + 1;
-	int right = MaxDepth(root.right) + 1;
-	return left > right ? left : right;
+
+	public class Solution2 {
+		public int MaxDepth(TreeNode root) {
+			if (root == null) return 0;
+			return Math.Max(MaxDepth(root.left), MaxDepth(root.right)) + 1;
+		}
+	}
+
+	public class Solution3 {
+		public int MaxDepth(TreeNode root) {
+			if (root == null) return 0;
+			int depth = 0;
+			Queue<TreeNode> queue = new Queue<TreeNode>();
+			queue.Enqueue(root);
+			while (queue.Count > 0) {
+				int count = queue.Count;
+				for (int i = 0; i < count; i++) {
+					TreeNode node = queue.Dequeue();
+					if (node.left != null) queue.Enqueue(node.left);
+					if (node.right != null) queue.Enqueue(node.right);
+				}
+				depth++;
+			}
+			return depth;
+		}
+	}
+
+	class Program {
+		static void Main(string[] args) {
+			Solution s = new Solution2();
+			int?[] arr = { 1, 2, 3, 4, 5, null, 6, 7, 8, null, null, 9, 10 };
+			TreeNode root = Tree.CreateTree(arr);
+			int res = s.MaxDepth(root);
+			Console.Write(res);
+		}
+	}
 }
