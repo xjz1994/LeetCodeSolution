0eb5cbdd7a6600ada4d248c2b46b805aa190e8b9
xjz1994 <504767600@qq.com>
2017-12-09 22:36:04 +0800

    482

---
+++ Solution/Javascript/482. License Key Formatting.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} S
+ * @param {number} K
+ * @return {string}
+ */
+var licenseKeyFormatting = function (S, K) {
+    let res = "";
+    let str = S.replace(/-/g, "");
+    let groupLength = 0;
+    for (let i = str.length - 1; i >= 0; i--) {
+        res = str[i].toUpperCase() + res;
+        if (++groupLength == k && i != 0) {
+            res = "-" + res;
+            groupLength = 0;
+        }
+    }
+    return res;
+};
+
+
+let s = "2-5g-3-J";
+let k = 2;
+let res = licenseKeyFormatting(s, k);
+console.log(res);
