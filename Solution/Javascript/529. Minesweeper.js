7bb1e6ece42a2ae2c03b627ebeae4891ceab551c
xjz1994 <504767600@qq.com>
2017-12-11 00:10:22 +0800

    扫雷

--- Solution/Javascript/529. Minesweeper.js
+++ Solution/Javascript/529. Minesweeper.js
@@ -4,28 +4,41 @@
  * @return {character[][]}
  */
 var updateBoard = function (board, click) {
-    if (board[click[0], click[1]] == "M") {
-        board[click[0], click[1]] = "X";
-    }
+    let pos = [[-1, -1], [-1, 0], [-1, 1], [0, -1], /*[0, 0],*/[0, 1], [1, -1], [1, -1], [1, 0], [1, 1]];
+
     let queue = [{ row: click[0], col: click[1] }];
     while (queue.length > 0) {
         let p = queue.shift();
-        let mineNum = getMineNum(board, p.row, p.col);
+        let mineNum = 0;
+        for (var i in pos) {
+            let r = p.row + pos[i][0], c = p.col + pos[i][1];
+            if (r >= 0 && r < board.length && c >= 0 && c < board[0].length)
+                if (board[r][c] == "M")++mineNum;
+        }
+        if (mineNum > 0) {
+            board[p.row][p.col] = mineNum.toString();
+            continue;
+        }
+        for (var i in pos) {
+            let r = p.row + pos[i][0], c = p.col + pos[i][1];
+            if (r >= 0 && r < board.length && c >= 0 && c < board[0].length)
+                if (board[r][c] == "E") {
+                    board[r][c] = "B";
+                    queue.push({ row: r, col: c });
+                }
+        }
+    }
+    if (board[click[0]][click[1]] == "M") {
+        board[click[0]][click[1]] = "X";
     }
     return board;
 };
 
-var getMineNum = function (board, row, col) {
-    let num = 0;
-    let pos = []
-}
-
-let board =
-    [['E', 'E', 'E', 'E', 'E'],
+let board = [
+    ['E', 'E', 'E', 'E', 'E'],
     ['E', 'E', 'M', 'E', 'E'],
     ['E', 'E', 'E', 'E', 'E'],
-    ['E', 'E', 'E', 'E', 'E']];
-
-let click = [3, 0];
-
+    ['E', 'E', 'E', 'E', 'E']
+];
+let click = [1, 2];
 let res = updateBoard(board, click);
