916c830d9775d820ba2c9badba2d1f6b4d2d5337
xjz1994 <504767600@qq.com>
2017-12-11 14:35:09 +0800

    扫雷

--- Solution/Javascript/529. Minesweeper.js
+++ Solution/Javascript/529. Minesweeper.js
@@ -4,7 +4,7 @@
  * @return {character[][]}
  */
 var updateBoard = function (board, click) {
-    if (board[click[0]][click[1]] == "M") {
+    if (board[click[0]][click[1]] == "M") {         //click mine
         board[click[0]][click[1]] = "X";
         return board;
     }
@@ -17,8 +17,9 @@ var updateBoard = function (board, click) {
         let mineNum = 0;
         for (var i in pos) {
             let r = p.row + pos[i][0], c = p.col + pos[i][1];
-            if (r >= 0 && r < board.length && c >= 0 && c < board[0].length)
-                if (board[r][c] == "M")++mineNum;
+            if (r >= 0 && r < board.length && c >= 0 && c < board[0].length) {
+                if (board[r][c] == "M") mineNum++;
+            }
         }
         if (mineNum > 0) {
             board[p.row][p.col] = mineNum.toString();
@@ -26,12 +27,23 @@ var updateBoard = function (board, click) {
         }
         for (var i in pos) {
             let r = p.row + pos[i][0], c = p.col + pos[i][1];
-            if (r >= 0 && r < board.length && c >= 0 && c < board[0].length)
+            if (r >= 0 && r < board.length && c >= 0 && c < board[0].length) {
                 if (board[r][c] == "E") {
                     board[r][c] = "B";
                     queue.push({ row: r, col: c });
                 }
+            }
         }
     }
     return board;
 };
+
+let board = [
+    ['E', 'E', 'E', 'E', 'E'],
+    ['E', 'E', 'M', 'E', 'E'],
+    ['E', 'E', 'E', 'E', 'E'],
+    ['E', 'E', 'E', 'E', 'E']
+];
+
+let click = [3, 0];
+let res = updateBoard(board, click);
