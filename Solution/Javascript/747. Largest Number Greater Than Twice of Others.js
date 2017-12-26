5593990ba1762c8ea201e2247c3c7e7fcf448a58
xjz1994 <504767600@qq.com>
2017-12-26 22:15:58 +0800

    747

---
+++ Solution/Javascript/747. Largest Number Greater Than Twice of Others.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var dominantIndex = function (nums) {
+    let max = -1;
+    let largestIndex = -1;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] > max) {
+            max = nums[i];
+            largestIndex = i;
+        }
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+        if (i != largestIndex && nums[i] * 2 > max) {
+            return -1;
+        }
+    }
+    return largestIndex;
+};
