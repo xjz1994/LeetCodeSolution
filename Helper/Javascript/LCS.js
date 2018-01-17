edb7bd625b7941528f42bed2f75a5f5c58914e3f
xjz1994 <504767600@qq.com>
2018-01-17 16:58:15 +0800

    LCS

--- Helper/Javascript/LCS.js
+++ Helper/Javascript/LCS.js
@@ -1,22 +1,26 @@
-let editDistance = (word1, word2) => {
+let LCS = (word1, word2) => {
     let lenA = word1.length;
     let lenB = word2.length;
     let d = createMatrix(lenB + 1, lenA + 1);
 
     for (let i = 0; i <= lenA; i++) {
-        d[i][0] = i;
+        d[i][0] = 0;
     }
 
     for (let i = 0; i <= lenB; i++) {
-        d[0][i] = i;
+        d[0][i] = 0;
     }
 
     for (let i = 1; i <= lenA; i++) {
         for (let j = 1; j <= lenB; j++) {
             if (word1[i - 1] == word2[j - 1]) {
-                d[i][j] = d[i - 1][j - 1];
+                d[i][j] = d[i - 1][j - 1] + 1;
             } else {
-                d[i][j] = Math.min(d[i - 1][j] + 1, d[i][j - 1] + 1, d[i - 1][j - 1] + 1);
+                if (d[i - 1][j] >= d[i][j - 1]) {
+                    d[i][j] = d[i - 1][j];
+                } else {
+                    d[i][j] = d[i][j - 1];
+                }
             }
         }
     }
@@ -49,4 +53,4 @@ let printMatrix = (matrix) => {
 
 let a = "abcde";
 let b = "abcd";
-console.log(editDistance(a, b));
+console.log(LCS(a, b));
