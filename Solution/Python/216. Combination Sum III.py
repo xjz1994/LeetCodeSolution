90ffe76fa46c7d2aad9c1dcb214648682eb4107c
xjz1994 <504767600@qq.com>
2018-01-26 16:41:13 +0800

    216. Combination Sum III

--- Solution/Python/216. Combination Sum III.py
+++ Solution/Python/216. Combination Sum III.py
@@ -5,3 +5,20 @@ class Solution:
         :type n: int
         :rtype: List[List[int]]
         """
+        res = []
+        def gen(l, k, n, start):
+            if k == len(l) and n == 0:
+                res.append(l[:])
+                return
+            for i in range(start, 10):
+                l.append(i)
+                gen(l, k, n - i, i + 1)
+                l.pop()
+                
+        gen([], k, n, 1)
+        return res
+
+
+s = Solution()
+res = s.combinationSum3(3, 7)
+print(res)
