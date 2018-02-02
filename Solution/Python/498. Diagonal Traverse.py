204998dbb57a4693f4e629b26861bc5049d5c35a
xjz1994 <504767600@qq.com>
2018-02-02 17:57:27 +0800

    498

---
+++ Solution/Python/498. Diagonal Traverse.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findDiagonalOrder(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: List[int]
+        """
+        res = []
+        1, 2, 3, 6, 9
+        return res
+
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+s = Solution()
+res = s.findDiagonalOrder(matrix)
+print(res)
