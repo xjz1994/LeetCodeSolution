cec19410befac4b91e9aacce8f7273f2585e932a
xjz1994 <504767600@qq.com>
2018-02-05 10:44:12 +0800

    优化空间复杂度

--- Solution/Python/498. Diagonal Traverse.py
+++ Solution/Python/498. Diagonal Traverse.py
@@ -7,41 +7,40 @@ class Solution:
         if not matrix or not matrix[0]:
             return []
         res = []
-        lines = []
+        isRe = 1
 
-        def addLine(row, col):
-            res = []
+        def addLine(row, col, isRe):
+            line = []
             while row < len(matrix) and col >= 0:
-                res.append(matrix[row][col])
+                line.append(matrix[row][col])
                 row += 1
                 col -= 1
-            lines.append(res)
 
-        for i in range(len(matrix[0])):
-            addLine(0, i)
-        for i in range(1, len(matrix)):
-            addLine(i, len(matrix[i]) - 1)
-
-        isRe = 1
-        for i in lines:
             l = len(res)
             if isRe:
-                res[l:l] = i[::-1]
+                res[l:l] = line[::-1]
             else:
-                res[l:l] = i
+                res[l:l] = line
+
+        for i in range(len(matrix[0])):
+            addLine(0, i, isRe)
             isRe ^= 1
+        for i in range(1, len(matrix)):
+            addLine(i, len(matrix[i]) - 1, isRe)
+            isRe ^= 1
+
         return res
 
 
-matrix = [
-    [3],
-    [2]
-]
 # matrix = [
-#     [1, 2, 3],
-#     [4, 5, 6],
-#     [7, 8, 9]
+#     [3],
+#     [2]
 # ]
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
 
 s = Solution()
 res = s.findDiagonalOrder(matrix)
