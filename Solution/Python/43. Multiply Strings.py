d29a9a2fd13eeaaff6cef90c6d70cea71c706344
xjz1994 <504767600@qq.com>
2018-02-07 15:25:15 +0800

    solution2

--- Solution/Python/43. Multiply Strings.py
+++ Solution/Python/43. Multiply Strings.py
@@ -1,4 +1,4 @@
-class Solution:
+class Solution1:
     def multiply(self, num1, num2):
         """
         :type num1: str
@@ -33,8 +33,22 @@ class Solution:
         return res.lstrip("0")
 
 
-s = Solution()
-num1 = "123"
-num2 = "0"
+class Solution2:
+    def multiply(self, num1, num2):
+        res = 0
+        for i in range(len(num1)):
+            res *= 10
+            n = int(num1[i])
+            temp_n = 0
+            for j in range(len(num2)):
+                temp_n *= 10
+                temp_n += int(num2[j]) * n
+            res += temp_n
+        return str(res)
+
+
+s = Solution1()
+num1 = "999"
+num2 = "999"
 res = s.multiply(num1, num2)
 print(res)
