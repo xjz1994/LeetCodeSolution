b4ef934b96dde8b857c27049827aafaf1b6d4a28
xjz1994 <504767600@qq.com>
2018-02-08 16:56:18 +0800

    539

---
+++ Solution/Python/539. Minimum Time Difference.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findMinDifference(self, timePoints):
+        """
+        :type timePoints: List[str]
+        :rtype: int
+        """
+
+
+s = Solution()
+timePoints = ["23:59", "00:00"]
+res = s.findMinDifference(timePoints)
+print(res)
