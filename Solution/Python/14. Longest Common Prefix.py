d4211788f4fb0f499f196833556949432a8c8b93
xjz1994 <504767600@qq.com>
2018-02-15 10:52:30 +0800

    14. Longest Common Prefix

---
+++ Solution/Python/14. Longest Common Prefix.py
@@ -0,0 +1,25 @@
+class Solution:
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs:
+            return ""
+
+        for i, group in enumerate(zip(*strs)):
+            if len(set(group)) > 1:
+                return strs[0][:i]
+
+        return min(strs)
+
+
+strs = [
+    "abcdefg",
+    "abcdefghijk",
+    "abcdfghijk",
+    "abcef"
+]
+s = Solution()
+res = s.longestCommonPrefix(strs)
+print(res)
