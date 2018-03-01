adeafec7d2b3d9798bf712f3f5072f395a4029e8
xjz1994 <504767600@qq.com>
2018-03-01 10:25:25 +0800

    40. Combination Sum II

--- Solution/Python/40. Combination Sum II.py
+++ Solution/Python/40. Combination Sum II.py
@@ -8,13 +8,19 @@ class Solution:
         res = []
         used = []
 
+        def count(l):
+            m = {}
+            for i in l:
+                m[i] = m.get(i, 0) + 1
+            return m
+
         def gen(nums, remain, index):
             if remain < 0:
                 return
             if remain == 0:
-                s = set(nums)
-                if s not in used:
-                    used.append(s)
+                m = count(nums)
+                if m not in used:
+                    used.append(m)
                     res.append(nums[:])
                 return
             for i in range(index, len(candidates)):
@@ -27,7 +33,11 @@ class Solution:
 
 
 s = Solution()
-candidates = [10, 1, 2, 7, 6, 1, 5]
-target = 8
+# candidates = [1, 1, 2, 3]
+# target = 3
+# candidates = [10, 1, 2, 7, 6, 1, 5]
+# target = 8
+candidates = [2, 5, 1, 1, 2, 3, 3, 3, 1, 2, 2]
+target = 5
 res = s.combinationSum2(candidates, target)
 print(res)
