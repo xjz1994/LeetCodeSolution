49830631a4c6bd0d53b480e5e2da0fc3cbcf9f9c
xjz1994 <504767600@qq.com>
2018-03-09 14:14:06 +0800

    746. Min Cost Climbing Stairs

---
+++ Solution/Python/746. Min Cost Climbing Stairs.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minCostClimbingStairs(self, cost):
+        """
+        :type cost: List[int]
+        :rtype: int
+        """
+        dp = [0] * len(cost)
+        dp[0] = cost[0]
+        dp[1] = cost[1]
+        for i in range(2, len(cost)):
+            dp[i] = cost[i] + min(dp[i - 1], dp[i - 2])
+        return min(dp[-1], dp[-2])
+
+
+s = Solution()
+cost = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1]
+res = s.minCostClimbingStairs(cost)
+print(res)
