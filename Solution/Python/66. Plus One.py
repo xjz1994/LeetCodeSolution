281162fa719dab326b50da061d92e6cee30ed55a
xjz1994 <504767600@qq.com>
2018-03-15 14:13:25 +0800

    py solution

---
+++ Solution/Python/66. Plus One.py
@@ -0,0 +1,19 @@
+class Solution:
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        for i in range(len(digits) - 1, -1, -1):
+            if digits[i] < 9:
+                digits[i] = digits[i] + 1
+                return digits
+            else:
+                digits[i] = 0
+        digits.insert(0, 1)
+        return digits
+
+
+s = Solution()
+res = s.plusOne([9, 9, 9])
+print(res)
