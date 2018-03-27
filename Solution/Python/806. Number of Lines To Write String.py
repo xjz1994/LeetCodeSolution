cb91e43c1c36b3eee44625f762aa3b4b16b58e0a
xjz1994 <504767600@qq.com>
2018-03-27 15:32:44 +0800

    806. Number of Lines To Write String

---
+++ Solution/Python/806. Number of Lines To Write String.py
@@ -0,0 +1,13 @@
+class Solution:
+    def numberOfLines(self, widths, S):
+        """
+        :type widths: List[int]
+        :type S: str
+        :rtype: List[int]
+        """
+        res, cur = 1, 0
+        for i in S:
+            width = widths[ord(i) - ord('a')]
+            res += 1 if cur + width > 100 else 0
+            cur = width if cur + width > 100 else cur + width
+        return [res, cur]
