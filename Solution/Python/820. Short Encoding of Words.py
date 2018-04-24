db6dd7c75a134fd62c1ea2dc32ffb2e06696a177
xjz1994 <504767600@qq.com>
2018-04-24 17:12:30 +0800

    minimumLengthEncoding

---
+++ Solution/Python/820. Short Encoding of Words.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minimumLengthEncoding(self, words):
+        """
+        :type words: List[str]
+        :rtype: int
+        """
+        root = dict()
+        return 1
+
+
+words = ["time", "me", "bell"]
+s = Solution()
+res = s.minimumLengthEncoding(words)
+print(res)
