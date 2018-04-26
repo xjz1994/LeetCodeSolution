4c49d3c2b65b834ecc325a9bdee9006cdff6de75
xjz1994 <504767600@qq.com>
2018-04-26 14:17:38 +0800

    isSameTree

---
+++ Solution/Python/100. Same Tree.py
@@ -0,0 +1,33 @@
+import sys
+sys.path.append(r'F:\xjzspace\LeetCodeSolution\Helper\Python')
+from Tree import *
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution:
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        if (p is None) and (q is None):
+            return True
+        if (p is None and q) or (q is None and p):
+            return False
+        if p.val != q.val:
+            return False
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+
+p = Tree.CreateTree([5, 1, 4, None, None, 3, 6])
+q = Tree.CreateTree([5, 1, 4, None, None, 3, 6])
+s = Solution()
+res = s.isSameTree(p, q)
+print(res)
