9461cfe178fa765185f7efda3951a739ce323a83
xjz1994 <504767600@qq.com>
2018-05-15 20:49:32 +0800

    114. Flatten Binary Tree to Linked List

--- Solution/Python/114. Flatten Binary Tree to Linked List.py
+++ Solution/Python/114. Flatten Binary Tree to Linked List.py
@@ -11,6 +11,34 @@ from Tree import *
 #         self.right = None
 
 
+# class Solution:
+#     def flatten(self, root):
+#         """
+#         :type root: TreeNode
+#         :type sum: int
+#         :rtype: List[List[int]]
+#         """
+#         if root == None:
+#             return None
+
+#         nodes = []
+
+#         def preOrder(node):
+#             nodes.append(node)
+#             if node.left:
+#                 preOrder(node.left)
+#             if node.right:
+#                 preOrder(node.right)
+
+#         preOrder(root)
+
+#         for i in range(len(nodes)):
+#             nodes[i].left = None
+#             if i != len(nodes) - 1:
+#                 nodes[i].right = nodes[i+1]
+#             else:
+#                 nodes[i].right = None
+
 class Solution:
     def flatten(self, root):
         """
@@ -21,24 +49,24 @@ class Solution:
         if root == None:
             return None
 
-        nodes = []
+        def helper(node):
+            if node == None:
+                return None
+            left = helper(node.left)
+            right = helper(node.right)
+            if left:
+                left.right = node.right
+                node.right = node.left
+                node.left = None
+            if right:
+                return right
+            if left:
+                return left
+            return node
 
-        def preOrder(node):
-            nodes.append(node)
-            if node.left:
-                preOrder(node.left)
-            if node.right:
-                preOrder(node.right)
-
-        preOrder(root)
-
-        for i in range(len(nodes)):
-            nodes[i].left = None
-            if i != len(nodes) - 1:
-                nodes[i].right = nodes[i+1]
-            else:
-                nodes[i].right = None
+        helper(root)
 
 
 root = Tree.CreateTree([1, 2, 5, 3, 4, None, 6])
 Solution().flatten(root)
+print(root)
