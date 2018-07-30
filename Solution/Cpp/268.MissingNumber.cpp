8e3e9ffc2bdd1d220bef4874f7e302943d2cecb1
xiejunzhao <xiejunzhao@yy.com>
2018-07-30 20:23:27 +0800

    gcc config

--- Solution/Cpp/268.MissingNumber.cpp
+++ Solution/Cpp/268.MissingNumber.cpp
@@ -1,9 +1,29 @@
-class Solution {
-    public:
-        int missingNumber(vector<int> & amp; nums) {
-            int x = 0;
-            for (int i = 0; i <= nums.size(); i++) x ^= i;
-            for (auto n: nums) x ^= n;
-            return x;
+#include <stdio.h>
+#include <iostream>
+#include <set>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+  public:
+    int missingNumber(vector<int> nums)
+    {
+        int x = 0;
+        for (int i = 0; i <= nums.size(); i++)
+        {
+            x ^= i;
         }
+        for (auto n : nums)
+        {
+            x ^= n;
+        }
+        return x;
+    }
 };
+
+int main()
+{
+    return 0;
+}
---
+++ gcc.bat
@@ -0,0 +1,1 @@
+bash -c "g++ -g /mnt/c/Users/admin/OneDrive/study/LeetCodeSolution/Solution/Cpp/%1 -o /mnt/c/Users/admin/Desktop/%2"
