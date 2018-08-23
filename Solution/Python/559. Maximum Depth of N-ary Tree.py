e0837456abd67d4c2750d294df51789486b048a2
xjz1994 <504767600@qq.com>
2018-08-23 20:58:31 +0800

    559. Maximum Depth of N-ary Tree

---
+++ Solution/Python/559. Maximum Depth of N-ary Tree.py
@@ -0,0 +1,18 @@
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val, children):
+        self.val = val
+        self.children = children
+"""
+
+
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: Node
+        :rtype: int
+        """
+        if not root:
+            return 0
+        return max([self.maxDepth(i) for i in root.children+[None]])+1
