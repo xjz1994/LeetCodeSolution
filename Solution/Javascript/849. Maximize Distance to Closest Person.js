b631879628371950e19c8f8812154a9b9ff3f3b1
xjz1994 <504767600@qq.com>
2018-12-03 15:35:00 +0800

    Update 849. Maximize Distance to Closest Person.js

--- Solution/Javascript/849. Maximize Distance to Closest Person.js
+++ Solution/Javascript/849. Maximize Distance to Closest Person.js
@@ -3,28 +3,37 @@
 * @return {number}
 */
 var maxDistToClosest = function (seats) {
-    let maxCoutinuteCount = 0;
-    let lastSeatIdx = -1
+    let continueArr = [];
+    let continueCount = 0;
     for (let i = 0; i < seats.length; i++) {
-        if (lastSeatIdx == -1 && seats[i] == 0) {
-            lastSeatIdx = i;
-        } else if (lastSeatIdx != -1 && seats[i] == 1) {
-            maxCoutinuteCount = Math.max(i - lastSeatIdx, maxCoutinuteCount);
-            lastSeatIdx = i;
+        if (seats[i] == 0) {
+            continueCount++;
+        }
+        if ((seats[i] == 1 && continueCount > 0) || i == seats.length - 1) {
+            continueArr.push(continueCount);
+            continueCount = 0;
         }
     }
 
-    let lastContinuteCount = 0
+    let arr = continueArr.map((val) => Math.ceil(val / 2))
+
+    let leftContinue = 0
+    if (seats[0] == 0) {
+        leftContinue = continueArr[0];
+    }
+    let rightContinue = 0
     if (seats[seats.length - 1] == 0) {
-        lastContinuteCount = (seats.length - 1) - lastSeatIdx;
+        rightContinue = continueArr[continueArr.length - 1];
     }
 
-    return Math.max(Math.ceil(maxCoutinuteCount / 2), lastContinuteCount);
+    return Math.max(leftContinue, rightContinue, Math.max.apply(null, arr));
 }
 
 // let seats = [1, 0, 0, 0, 1, 0, 1];
 // let seats = [1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0];
 // let seats = [0, 1]
 let seats = [0, 0, 1]
+// let seats = [1, 0, 0, 0]
+// let seats = [0, 1, 0, 1, 0]
 let res = maxDistToClosest(seats);
 console.log(res);
