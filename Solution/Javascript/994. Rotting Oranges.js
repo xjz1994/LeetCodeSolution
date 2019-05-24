5cb5348a74ef9c16748d3104cff0eba37e50abce
xjz1994 <504767600@qq.com>
2019-05-24 18:01:29 +0800

    Create 994. Rotting Oranges.js

---
+++ Solution/Javascript/994. Rotting Oranges.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var orangesRotting = function (grid) {
+    let queue = [];
+    queue.push({ x: 0, y: 0 });
+    while (queue.length > 0) {
+
+    }
+};
+
+let grid = [
+    [2, 1, 1],
+    [1, 1, 0],
+    [0, 1, 1]
+];
+let res = orangesRotting(grid);
+console.log(res);
