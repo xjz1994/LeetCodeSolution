9f022c89a2a089072cdaecf9bed385d7b25a8c70
xjz1994 <504767600@qq.com>
2020-08-23 19:10:04 +0800

    Create 1389. Create Target Array in the Given Order.js

---
+++ Solution/Javascript/1389. Create Target Array in the Given Order.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @param {number[]} index
+ * @return {number[]}
+ */
+var createTargetArray = function (nums, index) {
+    let res = []
+    for (let i = 0; i < nums.length; i++) {
+        res.splice(index[i], 0, nums[i])
+    }
+    return res;
+};
