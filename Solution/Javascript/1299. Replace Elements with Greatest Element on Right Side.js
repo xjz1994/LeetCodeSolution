4d5f805ba9d7bfce0a51dcd5f235a48671f0b59e
xjz1994 <504767600@qq.com>
2020-08-26 17:22:36 +0800

    Create 1299. Replace Elements with Greatest Element on Right Side.js

---
+++ Solution/Javascript/1299. Replace Elements with Greatest Element on Right Side.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} arr
+ * @return {number[]}
+ */
+var replaceElements = function (arr) {
+    let max = -1;
+    for (let i = arr.length - 1; i >= 0; i--) {
+        let temp = arr[i];
+        arr[i] = max;
+        max = Math.max(max, temp);
+    }
+    return arr;
+};
+
+let arr = [17, 18, 5, 4, 6, 1]
+arr = replaceElements(arr)
+console.log(arr);
