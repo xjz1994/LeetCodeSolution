1f27fc4e8ac6af2348c194f35e1581a6b3a5e2a7
xjz1994 <504767600@qq.com>
2020-09-02 19:46:21 +0800

    Create 1475. Final Prices With a Special Discount in a Shop.js

---
+++ Solution/Javascript/1475. Final Prices With a Special Discount in a Shop.js
@@ -0,0 +1,49 @@
+class Stack {
+    constructor() {
+        this.items = [];
+        this.count = 0;
+    }
+
+    empty() {
+        return this.count === 0;
+    }
+
+    push(item) {
+        this.items.push(item);
+        this.count = this.count + 1;
+    }
+
+    pop() {
+        if (this.count > 0) {
+            this.count = this.count - 1;
+        }
+
+        return this.items.pop();
+    }
+
+    top() {
+        return this.items.slice(-1)[0];
+    }
+}
+
+/**
+ * @param {number[]} prices
+ * @return {number[]}
+ */
+var finalPrices = function (prices) {
+    let res = new Array(prices.length).fill(0);
+    let stack = new Stack();
+    prices.push(0);
+    for (let i = 0; i < prices.length; i++) {
+        while (!stack.empty() && prices[i] <= prices[stack.top()]) {
+            res[stack.top()] = prices[stack.top()] - prices[i];
+            stack.pop();
+        }
+        stack.push(i);
+    }
+    return res;
+};
+
+let prices = [8, 4, 6, 2, 3];
+let res = finalPrices(prices);
+console.log(res);
