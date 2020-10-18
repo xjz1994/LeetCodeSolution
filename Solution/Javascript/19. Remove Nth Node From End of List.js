767c8b428b01fc75cbaeb8d8206de4cbe8b11393
xjz1994 <504767600@qq.com>
2020-10-18 22:23:31 +0800

    Create 19. Remove Nth Node From End of List.js

---
+++ Solution/Javascript/19. Remove Nth Node From End of List.js
@@ -0,0 +1,34 @@
+let LinkList = require("../../Helper/Javascript/LinkList").LinkList;
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function (head, n) {
+    let fast = head;
+    let slow = head;
+    for (let i = 0; i < n; i++) {
+        fast = fast.next;
+    }
+    if (fast === null) {
+        return head.next;
+    }
+    while (fast.next !== null) {
+        fast = fast.next;
+        slow = slow.next;
+    }
+    slow.next = slow.next.next;
+    return head;
+};
+
+let list = LinkList.CreateLinkList([1, 2, 3, 4, 5]);
+removeNthFromEnd(list, 2);
+LinkList.Walk(list);
