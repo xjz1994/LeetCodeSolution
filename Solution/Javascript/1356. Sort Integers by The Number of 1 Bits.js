8b881ab1ef6665889665f2ca3285422e38367a29
xjz1994 <504767600@qq.com>
2020-11-06 11:21:34 +0800

    Update 1356. Sort Integers by The Number of 1 Bits.js

--- Solution/Javascript/1356. Sort Integers by The Number of 1 Bits.js
+++ Solution/Javascript/1356. Sort Integers by The Number of 1 Bits.js
@@ -2,12 +2,36 @@
  * @param {number[]} arr
  * @return {number[]}
  */
+// var sortByBits = function (arr) {
+//     arr.sort((a, b) => {
+//         let aArr = a.toString(2).match(/1/g);
+//         let bArr = b.toString(2).match(/1/g);
+//         let aNum = aArr ? aArr.length : 0;
+//         let bNum = bArr ? bArr.length : 0;
+//         if (aNum !== bNum) {
+//             return aNum - bNum;
+//         } else {
+//             return a - b;
+//         }
+//     });
+//     return arr
+// };
+
+
 var sortByBits = function (arr) {
+    let hammingWeight = (n) => {
+        let sum = 0;
+        while (n > 0) {
+            if (n & 1 == 1) {
+                sum++;
+            }
+            n = n >>> 1;
+        }
+        return sum;
+    }
     arr.sort((a, b) => {
-        let aArr = a.toString(2).match(/1/g);
-        let bArr = b.toString(2).match(/1/g);
-        let aNum = aArr ? aArr.length : 0;
-        let bNum = bArr ? bArr.length : 0;
+        let aNum = hammingWeight(a)
+        let bNum = hammingWeight(b);
         if (aNum !== bNum) {
             return aNum - bNum;
         } else {
